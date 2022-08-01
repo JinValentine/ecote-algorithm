21a0922557d05ccd6f2f9383bb6fd4c82d2352b9
JINUK JEONG <wlsdnr214@gmail.com>
2022-08-02 03:03:30 +0900

    Create 316p.py

---
+++ chapter11/316p.py
@@ -0,0 +1,35 @@
+# 시간이 적게 걸리는 음식부터 확인하는 탐욕적 접근 방식
+# 시간이 적게 걸리는 음식부터 제거해 나가는 방식을 이용하면 된다.
+# 우선순위 큐를 이용하여 구현이 가능하다.
+# https://school.programmers.co.kr/learn/courses/30/lessons/42891?language=python3 프로그래머스 사이트에서 테스트 가능
+import heapq
+
+def solution(food_times, k):
+    # 전체 음식을 먹는 시간보다 k가 크거나 같다면 -1
+    if sum(food_times) <= k:
+        return -1
+    
+    # 시간이 작은 음식부터 빼야 하므로 우선순위 큐를 이용
+    q = []
+    for i in range(len(food_times)):
+        # (음식 시간, 음식 번호) 형태로 우선순위 큐에 삽입
+        heapq.heappush(q, (food_times[i], i + 1))
+        
+    sum_value = 0 # 먹기 위해 사용한 시간
+    previous = 0 # 직전에 다 먹은 음식 시간
+    
+    length = len(food_times) # 남은 음식의 개수
+    
+    # sum_value + (현재의 음식 시간 - 이전 음식 시간) * 현재 음식 개수와 k 비교
+    # 이전 음식을 다 먹으면서 다른 음식도 previous 시간만큼 먹었기 때문에 빼줘야 한다.
+    while sum_value + ((q[0][0] - previous)  * length) <= k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previous) * length
+        length -= 1 # 다 먹은 음식 제외
+        previous = now # 이전 음식 시간 재설정 
+    
+    # 남은 음식 중에서 몇 번째 음식인지 확인하여 출력
+    # 매초 먹어야 할 음식들은 번호순으로 먹기 때문에 번호 기준으로 다시 정렬한다.
+    result = sorted(q, key = lambda x: x[1])
+    return result[(k - sum_value) % length][1]
+
